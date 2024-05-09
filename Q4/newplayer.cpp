eff9e8c2d38eb6a9bac64e084e98889afca777d7
Volodymyr Antoniuk <113469197+antoniukoff@users.noreply.github.com>
2024-05-09 14:08:39 -0400

    Update newplayer.cpp
    extra comments

--- Q4/newplayer.cpp
+++ Q4/newplayer.cpp
@@ -1,12 +1,12 @@
 void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 {
     Player* player = nullptr;
-    bool newPlayer = (player = g_game.getPlayerByName(recipient)) == nullptr;
+    bool newPlayer = (player = g_game.getPlayerByName(recipient)) == nullptr; // bool checking if the player was not found
 
-    if (newPlayer) {
+    if (newPlayer) { // if true will allocate a new one
         player = new Player(nullptr);
         if (!IOLoginData::loadPlayerByName(player, recipient)) {
-            delete player;
+            delete player; // delete newly allocated player if condition was false
             return;
         }
     }
@@ -14,7 +14,7 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
     Item* item = Item::CreateItem(itemId);
     if (!item) {
         if (newPlayer) {
-            delete player;
+            delete player; // if item was not created and delete the allocated player
         }
         return;
     }
@@ -23,7 +23,7 @@ void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 
     if (player->isOffline()) {
         IOLoginData::savePlayer(player);
-        delete player;
+        delete player; // if online free the resources of the player even if was valid on line 4
     }
     // I am guessing database object management will handle the deletion of player object
     // So need to delete player if it's online as loadPlayerByName() stores it in database with properly set up properties
